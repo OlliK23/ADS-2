bbd9077827ff0d593ca1c16a87f3243318574ae4
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-02 20:07:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,60 @@
 
 
 double pown(double value, uint16_t n) {
+    if (n == 0)return 1;
+    double dat = 1.0;
 
+    while (n > 0) {
+        if (n & 1) {
+            dat *= value;
+        }
+
+        value *= value;
+        n >>= 1;
+    }
+    return dat;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n == 0)return 1;
+    return fact(n - 1) * n;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double res = x;
+    return pown(res, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double dat = 1;
+    while (count > 0) {
+        dat += calcItem(x, count);
+        --count;
+    }
+    return dat;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double result = 0;
+    while (count > 0) {
+        if(count%2==0)
+        result -= calcItem(x, 2*count-1);
+        else{
+            result += calcItem(x, 2 * count - 1);}
+        --count;
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double result = 0;
+    while (count > 0) {
+        if (count % 2 == 0)
+            result -= calcItem(x, 2 * count - 2);
+        else {
+            result += calcItem(x, 2 * count - 2);
+        }
+        --count;
+    }
+    return result;
 }
